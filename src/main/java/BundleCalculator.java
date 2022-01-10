0d63963abe8f0c03b9f8854197ef60fb669f5b1e
Zhifeng He <homoumoumou@gmail.com>
2022-01-10 21:44:54 +1000

    naming convention

--- src/main/java/BundleCalculator.java
+++ src/main/java/BundleCalculator.java
@@ -1,20 +1,25 @@
 import java.util.*;
+
 import org.apache.log4j.Logger;
 
 public class BundleCalculator {
 
 
-    public double Calculate(int postNums, String formatCode ){
+    public static void main(String[] args) {
+        BundleCalculator bundleCalculator = new BundleCalculator();
+        bundleCalculator.Calculate(14, "FLAC");
+    }
+
+    public double Calculate(int postNums, String formatCode) {
         // initial
         int bundleNum;
         int remind = postNums;
         double price;
         double totoalPrice = 0;
-        final Logger logger = Logger.getLogger(BundleCalculator.class);
         final Logger savePriceLog = Logger.getLogger("savePriceLog");
 
         // create bundles table
-        bundles bundles = new bundles(formatCode);
+        Bundles bundles = new Bundles(formatCode);
         // get Bundle Table
         Map bundleTable = bundles.getBundleTable();
         // get the keyArray
@@ -22,27 +27,28 @@ public class BundleCalculator {
         // get the bundleKeySet
         Set<Integer> bundleKeySet = getBundleKeySet(keyArray, postNums);
 
-        Iterator  ite = bundleKeySet.iterator();
+        Iterator ite = bundleKeySet.iterator();
         //  the element in set is unrepeatable, then {5,5,3} => {5,3}
-        while(ite.hasNext()){
-            int key = (int)ite.next();
-            if(((remind / key) > 0)){
+        while (ite.hasNext()) {
+            int key = (int) ite.next();
+            if (((remind / key) > 0)) {
                 bundleNum = remind / key;
                 remind = remind - bundleNum * key;
                 price = bundleNum * Double.parseDouble(String.valueOf(bundleTable.get(key)));
-                savePriceLog.info(bundleNum + " * " + key + " $"+price);
+                savePriceLog.info(bundleNum + " * " + key + " $" + price);
+                System.out.println(bundleNum + " * " + key + " $" + price);
                 totoalPrice += price;
             } else {
                 // if reminding post can`t make as bundles, find the closet bundles as compensation
-                if (remind > 0){
-                    Iterator  minIte = bundleKeySet.iterator();
+                if (remind > 0) {
+                    Iterator minIte = bundleKeySet.iterator();
                     int minKey = 0;
                     double minMargin = 9999;
-                    while(minIte.hasNext()){
+                    while (minIte.hasNext()) {
                         // 14 - 9 = 5  5-6 = -1
-                        int tempKey = (int)minIte.next();
+                        int tempKey = (int) minIte.next();
                         double tempMargin = remind - tempKey;
-                        if(Math.abs(minMargin) > Math.abs(tempMargin) && tempMargin<0){
+                        if (Math.abs(minMargin) > Math.abs(tempMargin) && tempMargin < 0) {
                             minKey = tempKey;
                             minMargin = tempMargin;
                         }
@@ -50,24 +56,26 @@ public class BundleCalculator {
                     bundleNum = 1;
                     remind = 0;
                     price = bundleNum * Double.parseDouble(String.valueOf(bundleTable.get(minKey)));
-                    savePriceLog.info(bundleNum + " * " + key + " $"+price);
+                    System.out.println(bundleNum + " * " + key + " $" + price);
+                    savePriceLog.info(bundleNum + " * " + key + " $" + price);
                     totoalPrice += price;
                 }
             }
-            if(!ite.hasNext() && (remind % key)!=0){
+            if (!ite.hasNext() && (remind % key) != 0) {
                 price = Double.parseDouble(String.valueOf(bundleTable.get(key)));
-                savePriceLog.info(1 + " * " + key + " $"+price);
+                System.out.println(1 + " * " + key + " $" + price);
+                savePriceLog.info(1 + " * " + key + " $" + price);
                 totoalPrice += price;
             }
         }
-        savePriceLog.info(postNums +" "+ formatCode + " $" +totoalPrice);
+        savePriceLog.info(postNums + " " + formatCode + " $" + totoalPrice);
+        System.out.println(postNums + " " + formatCode + " $" + totoalPrice);
 
-       return totoalPrice;
+        return totoalPrice;
     }
 
-
-    private int[] getKeySet(Map bundleTable){
-        Set keySet= bundleTable.keySet();
+    private int[] getKeySet(Map bundleTable) {
+        Set keySet = bundleTable.keySet();
         Set<Integer> reverseSets = new TreeSet<>(Comparator.reverseOrder());
         Iterator ite = keySet.iterator();
         // sort the key set (decrease)
@@ -84,12 +92,13 @@ public class BundleCalculator {
 
     private Set<Integer> getBundleKeySet(int[] keyArray, int postNums) {
         Set<Integer> reverseSets = new TreeSet<>(Comparator.reverseOrder());
-        for (int key: keyArray) {
+        for (int key : keyArray) {
             reverseSets.add(key);
         }
-        labe:for(int j= 0; j < keyArray.length;j++){
-            int first_remind = postNums-keyArray[j];
-            for(int i= 0; i < keyArray.length; i++) {
+        labe:
+        for (int j = 0; j < keyArray.length; j++) {
+            int first_remind = postNums - keyArray[j];
+            for (int i = 0; i < keyArray.length; i++) {
                 if (first_remind % keyArray[i] == 0) {
                     reverseSets.clear();
                     reverseSets.add(keyArray[j]);
@@ -97,8 +106,8 @@ public class BundleCalculator {
                     break labe;
                 } else {
                     int second_remind = first_remind - keyArray[i];
-                    for (int k = 0; k < keyArray.length; k++){
-                        if(second_remind > 0 && second_remind % keyArray[k] == 0){
+                    for (int k = 0; k < keyArray.length; k++) {
+                        if (second_remind > 0 && second_remind % keyArray[k] == 0) {
                             reverseSets.clear();
                             reverseSets.add(keyArray[j]);
                             reverseSets.add(keyArray[i]);
@@ -110,11 +119,5 @@ public class BundleCalculator {
             }
         }
         return reverseSets;
-    }
-
-
-    public static void main(String[] args) {
-        BundleCalculator bundleCalculator = new BundleCalculator();
-        bundleCalculator.Calculate(14, "FLAC");
     }
 }
--- src/main/java/Bundles.java
+++ src/main/java/Bundles.java
@@ -1,34 +1,35 @@
 import java.util.HashMap;
 import java.util.Map;
+
 import lombok.*;
 
 @Getter
 @Setter
-public class bundles {
+public class Bundles {
 
     private Map bundleTable;
 
 
-    public bundles(String formatCode) {
+    public Bundles(String formatCode) {
         switch (formatCode) {
             case "IMG":
                 Map imgTable = new HashMap();
-                imgTable.put(10,800);
-                imgTable.put(5,450);
+                imgTable.put(10, 800);
+                imgTable.put(5, 450);
                 this.bundleTable = imgTable;
                 break;
             case "FLAC":
                 Map audioTable = new HashMap();
-                audioTable.put(3,427.50);
-                audioTable.put(6,810);
-                audioTable.put(9,1147.50);
+                audioTable.put(3, 427.50);
+                audioTable.put(6, 810);
+                audioTable.put(9, 1147.50);
                 this.bundleTable = audioTable;
                 break;
             case "VID":
                 Map videoTable = new HashMap();
-                videoTable.put(3,570);
-                videoTable.put(5,900);
-                videoTable.put(9,1530);
+                videoTable.put(3, 570);
+                videoTable.put(5, 900);
+                videoTable.put(9, 1530);
                 this.bundleTable = videoTable;
                 break;
         }
