d1e5078f5d293fa02fe3cc0418acbbf460f19de8
Zhifeng He <homoumoumou@gmail.com>
2022-01-11 20:39:48 +1000

    OOP

---
+++ src/main/java/Demo.java
@@ -0,0 +1,18 @@
+import java.util.Map;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numbers of posts");
+        int postNums = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter format code");
+        String formatCode = scanner.nextLine();
+        Order order = new Order();
+        order.setPostNums(postNums);
+        order.setFormatCode(formatCode);;
+        order.Calculate();
+
+    }
+}
---
+++ src/main/java/Order.java
@@ -0,0 +1,24 @@
+import lombok.*;
+
+import java.util.Map;
+
+@Data
+public class Order {
+    private int postNums;
+    private String formatCode;
+    private Map bundleTable;
+
+    public Map getBundleTable(){
+        // create bundles table
+        Bundles bundles = new Bundles(formatCode);
+        // get Bundle Table
+        Map bundleTable = bundles.getBundleTable();
+        return bundleTable;
+    }
+    
+    public double Calculate(){
+        BundleCalculator bundleCalculator = new BundleCalculator();
+        return bundleCalculator.Calculate(postNums, formatCode);
+    }
+
+}
